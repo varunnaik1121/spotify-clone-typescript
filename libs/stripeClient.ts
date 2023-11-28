5de6174d89ee5ccb6e52266b4bb1fd33483a8b30
varunnaik1121 <varunnaik1121@gmail.com>
2023-11-28 17:35:10 +0530

    feat:updated stripe helpers

--- libs/stripeClient.ts
+++ libs/stripeClient.ts
@@ -1,9 +1,14 @@
-import {loadStripe,Stripe} from "@stripe/stripe-js"
-let stripePromise:Promise<Stripe|null>;
-export const getStripe=()=>{
-    if(!stripePromise){
-        stripePromise=loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY??'')
-    }
+import { loadStripe, Stripe } from '@stripe/stripe-js';
 
-    return stripePromise
-}
+let stripePromise: Promise<Stripe | null>;
+
+export const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY ??
+      ''
+    );
+  }
+
+  return stripePromise;
+};
